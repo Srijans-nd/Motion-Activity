2b59bb16e20489f6d39b80a5bf4740c30b43b16b
Srijan <srijan.srivastava@netradyne.com>
2019-02-19 12:23:24 +0530

    Initial Commit

---
+++ CoreMotionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotionDemo
+//
+//  Created by srijans on 19/02/19.
+//  Copyright © 2019 srijan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
